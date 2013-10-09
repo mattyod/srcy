baf6e74e69302d3cc56a4fc0d2f78de20b668990
mattyod <mattyod@gmail.com>
2013-10-09 11:42:06 +0100

    init creates conf file wherever you are

--- bin/args/init.js
+++ bin/args/init.js
@@ -1,22 +1,36 @@
 'use strict';
 var fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    log = require('../../lib/log');
+
+var getConf = function () {
+    var target = process.mainModule.filename.split('/');
+    target.pop();
+    return path.join(target.join('/'), 'resources/srcy.conf.js');
+};
+
+var isForced = function () {
+    var force = false;
+
+    if (process.argv[3] && process.argv[3].match(/-f/)) {
+        force = true;
+    }
+
+    return force;
+};
 
 module.exports = (function () {
-    var conf,
-        force = process.argv.slice(3);
+    var conf = fs.readFileSync(getConf(), 'utf8');
 
-    conf = fs.readFileSync(path.join(process.cwd(), 'bin/resources/srcy.conf.js'), 'utf8');
-
-    if (!force.length && fs.existsSync('srcy.conf.js')) {
-        console.log('Srcy: srcy.conf file already exists');
-        console.log('Srcy: use "srcy init -f" to over-write it with a blank conf file');
+    if (!isForced() && fs.existsSync('srcy.conf.js')) {
+        log.warn('srcy.conf file already exists');
+        log.info('use "srcy init -f" to over-write it with a blank conf file');
         process.exit(1);
     }
 
-    fs.writeFileSync('srcy.conf.js', conf.toString(), 'utf8');
+    fs.writeFileSync('srcy.conf.js', conf, 'utf8');
 
-    console.log('Srcy: blank srcy.conf.js file created');
+    log.success('blank srcy.conf.js file created');
 
     process.exit(0);
 
