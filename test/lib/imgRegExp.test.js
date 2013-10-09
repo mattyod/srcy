6492889695f69709cc3030c40fc0160696d8fde4
mattyod <mattyod@gmail.com>
2013-10-10 00:11:51 +0100

    start of regexp tests

---
+++ test/lib/imgRegExp.test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var imgRegExp = require('../../lib/imgRegExp');
+
+describe('imgRegExp', function() {
+    var reg,
+        conf = {
+            images: {
+                types: ['gif', 'png'],
+                folders: ['img', 'images']
+            }
+        };
+
+    beforeEach(function () {
+        reg = imgRegExp(conf);
+    });
+
+    it('matches a gif in the img folder', function () {
+        'img/path/thing.gif'.match(reg).should.be.ok;
+        '/img/path/thing.gif'.match(reg).should.be.ok;
+        'img/{variable}/thing.gif'.match(reg).should.be.ok;
+    });
+
+    it('matches a png in the img folder', function () {
+        'img/path/thing.png'.match(reg).should.be.ok;
+        '/img/path/thing.png'.match(reg).should.be.ok;
+        'img/{variable}/thing.png'.match(reg).should.be.ok;
+    });
+
+    it('matches a gif in the images folder', function () {
+        'images/path/thing.gif'.match(reg).should.be.ok;
+        '/images/path/thing.gif'.match(reg).should.be.ok;
+        'images/{variable}/thing.gif'.match(reg).should.be.ok;
+    });
+
+    it('matches a png in the images folder', function () {
+        'images/path/thing.png'.match(reg).should.be.ok;
+        '/images/path/thing.png'.match(reg).should.be.ok;
+        'images/{variable}/thing.png'.match(reg).should.be.ok;
+    });
+
+    it('does not match these things', function () {
+        console.log('-images/{variable}/thing.png'.match(reg));
+        (!'images/path/thing.pnga'.match(reg)).should.be.ok;
+        (!'rimages/path/thing.png'.match(reg)).should.be.ok;
+        (!'-images/{variable}/thing.png'.match(reg)).should.be.ok;
+        (!'images/path/thing.png-asd'.match(reg)).should.be.ok;
+    });
+
+});
