a7eb6b680f7bd25401e3073a4c125988a7f4ec24
mattyod <mattyod@gmail.com>
2013-10-09 23:09:51 +0100

    getConf tests

--- test/lib/getConf.test.js
+++ test/lib/getConf.test.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var getConf = require('../../lib/getConf'),
-    log = require('../../lib/log');
+    log = require('../../lib/log'),
+    fs = require('fs');
 
 describe('getConf', function () {
 
@@ -26,6 +27,18 @@ describe('getConf', function () {
 
         beforeEach(function () {
 
+            this.stub(fs, 'existsSync', function () {
+                return true;
+            });
+
+            this.stub(fs, 'readFileSync', function () {
+                return '{ "foo": "bar" }';
+            });
+
+        });
+
+        it('returns the parsed conf file', function () {
+            getConf.fetch().should.deep.equal(JSON.parse('{ "foo": "bar" }'));
         });
     });
 
