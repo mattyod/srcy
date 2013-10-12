0db88c496344a914ef9119550fca17a925a6edb7
mattyod <mattyod@gmail.com>
2013-10-12 18:09:53 +0100

    itterate tests

---
+++ test/lib/itterate.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var itterate = require('../../lib/itterate');
+
+describe('itterate', function () {
+    var callback,
+        obj = {
+            item1: 'foo',
+            item2: 'bar'
+        };
+
+    beforeEach(function () {
+        callback = this.spy();
+        itterate(obj, callback);
+    });
+
+    it('Executes the callback for each itteration passing value and key', function () {
+        callback.should.have.been.calledTwice;
+        callback.firstCall.args[0].should.equal('foo');
+        callback.firstCall.args[1].should.equal('item1');
+        callback.secondCall.args[0].should.equal('bar');
+        callback.secondCall.args[1].should.equal('item2');
+    });
+
+});
